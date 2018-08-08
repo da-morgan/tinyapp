7b04bd8351b22603ca518c880616a41b22d75cf7
David Morgan <david.alexandermorgan@gmail.com>
2018-08-08 01:37:52 +0000

    added comments

--- express_server.js
+++ express_server.js
@@ -16,24 +16,30 @@ function generateRandomString(length) {
     return result;
 }
 
+//database of URLs
 var urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xK": "http://www.google.com",
 }
 
+/* prints Database of URLs in the browser */
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
 });
 
+/* Displays lit of URLs currently in the urlDatabas object. */
 app.get("/urls", (req, res) => {
     let templateVars = {urls: urlDatabase};
     res.render('urls_index', templateVars);
 })
 
+/* Takes user to the form to input a domain */
 app.get("/urls/new", (req, res) => {
     res.render("urls_new");
 });
 
+/* Displays info about long and short URLS given
+   input of short URL.*/
 app.get("/urls/:id", (req, res) => {
     let templateVars = {
         shortURLS: req.params.id,
@@ -42,9 +48,13 @@ app.get("/urls/:id", (req, res) => {
     res.render('urls_show', templateVars);
 })
 
+
+/* user can input /u/<shortURL> and it directs them to 
+   the website it refers to in the local object of URL databases" */
 app.get("/u/:shortURL", (req, res) => {
     let longURL = urlDatabase[req.params.shortURL];
     console.log(urlDatabase[req.params.shortURL]);
+    res.statusCode = 301;
     res.redirect(longURL);
   });
 
@@ -58,17 +68,21 @@ app.post("/urls", (req, res) => {
     //console.log('string: ' + generated);
     //console.log(urlDatabase);
     //console.log('req.body.longURL: ' + req.body.longURL);
+    res.statusCode = 303
     res.redirect(`http://localhost:8080/urls/${generated}`);
 });
 
+//prints HTML when you type /hello
 app.get("/hello", (req, res) => {
     res.end("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+//prints Hello! when user visits the root page.
 app.get("/", (req, res) => {
     res.end("Hello!");
 });
 
+//Listens on port provided at the top of the file.
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}.`);
 })
